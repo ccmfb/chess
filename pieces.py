65d4394f49f92aef194c96b139babd8a36142e85
ccmfb <79724475+ccmfb@users.noreply.github.com>
2021-09-04 13:13:42 +0200

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -14,8 +14,49 @@ class Piece:
 		self.moves = []
 		self.move_rects = []
 
+		self.x_bound_left = 0
+		self.x_bound_right = 480
+		self.y_bound_top = 0
+		self.y_bound_bottom = 480
+
+		self.font = pygame.font.SysFont('Helvetica Neue', 15)
+
 	def update_moves(self, all_pieces):
-		pass
+		self.move_rects = []
+
+		for mov_type in self.moves:						
+			for mov in mov_type:
+
+				# Check for board boundaries
+				if self.x_bound_left < self.piece_rect.x + mov[0] < self.x_bound_right:
+					if self.y_bound_top < self.piece_rect.y + mov[1] < self.y_bound_bottom:
+						self.move_rects.append(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
+
+		for mov_type in self.moves:
+			for mov in mov_type:
+				for team in all_pieces:
+					for type_of_piece in team:
+						for piece in type_of_piece:
+							
+							# Check for other pieces
+							if self.piece_rect.x + mov[0] == piece.piece_rect.x and self.piece_rect.y + mov[1] == piece.piece_rect.y:
+								print('collides with: (', piece.piece_rect.x, ', ', piece.piece_rect.y, ')')
+								if piece.piece_rect in self.move_rects:
+									print('removed smt')
+									self.move_rects.remove(piece.piece_rect)
+									self.remove_further_moves(mov)
+
+	def remove_further_moves(self, move):
+		to_delete_moves = []
+		for mov_type in self.moves:
+			if move in mov_type:
+				index = mov_type.index(move)
+				to_delete_moves = mov_type[index:]
+
+		print('moves delete: ', to_delete_moves)
+		for mov in to_delete_moves:
+			if pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30) in self.move_rects:
+				self.move_rects.remove(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
 
 	def move(self, all_white, all_black, to_x, to_y):	
 		self.piece_rect.x = to_x
@@ -23,6 +64,7 @@ class Piece:
 		
 	def draw(self, screen):
 		pygame.draw.rect(screen, self.colour_rgb, self.piece_rect)
+		screen.blit(self.textsurf,(self.piece_rect.x + 7, self.piece_rect.y + 7))
 
 		if self.showing_moves:
 			for rect in self.move_rects:
@@ -32,200 +74,113 @@ class Pawn(Piece):
 	def __init__(self, position, colour):
 		super().__init__(position, colour)
 		
+		# 2 Possible ways of movement (normal, beating enemy)
+		self.moves = [[],[]]
+
 		if colour == 'w':
-			self.moves.append((0,60))
+			self.moves[0].append((0,60))
 		elif colour == 'b':
-			self.moves.append((0,-60))
+			self.moves[0].append((0,-60))
 
-	def update_moves(self, all_pieces):
-		self.move_rects = []
-		self.x_bound_left = 80
-		self.x_bound_right = 500
-		self.y_bound_top = 90
-		self.y_bound_bottom = 510
-
-		for mov in self.moves:
-			if self.piece_rect.x + mov[0] + 15 >= 80 and self.piece_rect.x + mov[0] + 15 <= 500:
-				if self.piece_rect.y + mov[1] + 15 >= 90 and self.piece_rect.y + mov[1] + 15 <= 510:
-					for lis in all_pieces[0]:
-						for piece in lis:
-							if self.piece_rect.x + mov[0] != piece.piece_rect.x and self.piece_rect.y + mov[1] != piece.piece_rect.y:
-								self.move_rects.append(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
-
-
+		self.textsurf = self.font.render('P', False, (0,179,255))
 
 class Rock(Piece):
 	def __init__(self, position, colour):
 		super().__init__(position, colour)
 
+		# 4 Possible ways of movement
+		self.moves = [
+				[],[],[],[]
+			]
+
 		for i in range(7):
-			self.moves.append((60 * (i+1), 0))
-			self.moves.append((-60 * (i+1), 0))
-			self.moves.append((0, 60 * (i+1)))
-			self.moves.append((0, -60 * (i+1)))
+			self.moves[0].append((60 * (i+1), 0))
+			self.moves[1].append((-60 * (i+1), 0))
+			self.moves[2].append((0, 60 * (i+1)))
+			self.moves[3].append((0, -60 * (i+1)))
 
-	def update_moves(self, all_pieces):
-		self.move_rects = []
-		self.x_bound_left = 80
-		self.x_bound_right = 500
-		self.y_bound_top = 90
-		self.y_bound_bottom = 510
-
-		self.find_x_bound_left(all_pieces[0], all_pieces[1])
-		self.find_x_bound_right(all_pieces[0], all_pieces[1])
-		self.find_y_bound_top(all_pieces[0], all_pieces[1])
-		self.find_y_bound_bottom(all_pieces[0], all_pieces[1])
-		
-		print('Bounds: ', self.x_bound_left, ', ', self.x_bound_right, ', ', self.y_bound_top, ', ', self.y_bound_bottom)
-	
-		for mov in self.moves:
-
-			if self.piece_rect.x + mov[0] + 15 >= self.x_bound_left and self.piece_rect.x + mov[0] + 15 <= self.x_bound_right:
-				if self.piece_rect.y + mov[1] + 15 < self.y_bound_bottom and self.piece_rect.y + mov[1] + 15 >= self.y_bound_top:
-
-					# Append possible moves
-					self.move_rects.append(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
-
-	def find_x_bound_left(self, all_white, all_black):
-		if self.colour == 'w':
-			team = all_white
-		else:
-			team = all_black
-
-		for mov in self.moves:
-
-			# Find x_bound_left
-			for lis in team:
-				for piece in lis:
-
-					# Collision with other whites on x
-					if self.piece_rect.x + mov[0] == piece.piece_rect.x:
-
-						# left bound
-						if self.piece_rect.x > piece.piece_rect.x:
-							self.x_bound_left = piece.piece_rect.x + 30
-
-	def find_x_bound_right(self, all_white, all_black):
-		if self.colour == 'w':
-			team = all_white
-		else:
-			team = all_black
-
-		for mov in self.moves:
-
-			# Find x_bound_right
-			for lis in team:
-				for piece in lis:
-
-					# Collision with other whites on x
-					if self.piece_rect.x + mov[0] == piece.piece_rect.x:
-						# Right bound
-						if self.piece_rect.x < piece.piece_rect.x:
-							self.x_bound_right = piece.piece_rect.x + 15
-
-	def find_y_bound_top(self, all_white, all_black):
-		if self.colour == 'w':
-			team = all_white
-		else:
-			team = all_black
-
-		for mov in self.moves:
-
-			# Find y_bound_top
-			for lis in team:
-				for piece in lis:
-
-					# Collision with other whites on y
-					if self.piece_rect.y + mov[1] == piece.piece_rect.y:
-
-						# top bound
-						if self.piece_rect.y > piece.piece_rect.y:
-							self.y_bound_top = piece.piece_rect.y + 15
-						
-
-	def find_y_bound_bottom(self, all_white, all_black):
-		if self.colour == 'w':
-			team = all_white
-		else:
-			team = all_black
-
-		for mov in self.moves:
-
-			# Find y_bound_bottom
-			for lis in team:
-				for piece in lis:
-
-					# Collision with other whites on y
-					if self.piece_rect.y + mov[1] == piece.piece_rect.y:
-
-						# bottom bound
-						if self.piece_rect.y < piece.piece_rect.y:
-							self.y_bound_bottom = piece.piece_rect.y + 15
+		self.textsurf = self.font.render('R', False, (0,179,255))
 
 class Knight(Piece):
 	def __init__(self, position, colour):
 		super().__init__(position, colour)
 
-		self.moves.append((60, -120))
-		self.moves.append((120, -60))
-		self.moves.append((120, 60))
-		self.moves.append((60, 120))
-		self.moves.append((-60, 120))
-		self.moves.append((-120, 60))
-		self.moves.append((-120, -60))
-		self.moves.append((-60, -120))
+		# 8 Possible ways of movement
+		self.moves = [
+				[],[],[],[],[],[],[],[]
+			]
 
-	def update_moves(self, all_pieces):
-		self.move_rects = []
-		for mov in self.moves:
-			self.move_rects.append(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
+		self.moves[0].append((60, -120))
+		self.moves[1].append((120, -60))
+		self.moves[2].append((120, 60))
+		self.moves[3].append((60, 120))
+		self.moves[4].append((-60, 120))
+		self.moves[5].append((-120, 60))
+		self.moves[6].append((-120, -60))
+		self.moves[7].append((-60, -120))
+
+
+		self.textsurf = self.font.render('Kn', False, (0,179,255))
 
 class Bishop(Piece):
 	def __init__(self, position, colour):
 		super().__init__(position, colour)
 
-		for i in range(7):
-			self.moves.append((60*(i+1),60*(i+1)))
-			self.moves.append((-60*(i+1),60*(i+1)))
-			self.moves.append((60*(i+1),-60*(i+1)))
-			self.moves.append((-60*(i+1),-60*(i+1)))
+		# 4 Possible movements
+		self.moves = [
+				[],[],[],[]
+			]
 
-	def update_moves(self, all_pieces):
-		self.move_rects = []
-		for mov in self.moves:
-			self.move_rects.append(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
+		for i in range(7):
+			self.moves[0].append((60*(i+1),60*(i+1)))
+			self.moves[1].append((-60*(i+1),60*(i+1)))
+			self.moves[2].append((60*(i+1),-60*(i+1)))
+			self.moves[3].append((-60*(i+1),-60*(i+1)))
+
+		self.textsurf = self.font.render('B', False, (0,179,255))
 
 class Queen(Piece):
 	def __init__(self, position, colour):
 		super().__init__(position, colour)
+		self.textsurf = self.font.render('Q', False, (0,179,255))
+
+		# 8 Possible types of movement
+		self.moves = [
+				[],[],[],[],[],[],[],[]
+			]
 
 		for i in range(7):
-			self.moves.append((60*(i+1),60*(i+1)))
-			self.moves.append((-60*(i+1),60*(i+1)))
-			self.moves.append((60*(i+1),-60*(i+1)))
-			self.moves.append((-60*(i+1),-60*(i+1)))
+			self.moves[0].append((60*(i+1),-60*(i+1)))
+			self.moves[1].append((60*(i+1),60*(i+1)))
+			self.moves[2].append((-60*(i+1),60*(i+1)))
+			self.moves[3].append((-60*(i+1),-60*(i+1)))
 
 		for i in range(7):
-			self.moves.append((60 * (i+1), 0))
-			self.moves.append((-60 * (i+1), 0))
-			self.moves.append((0, 60 * (i+1)))
-			self.moves.append((0, -60 * (i+1)))
+			self.moves[4].append((60 * (i+1), 0))
+			self.moves[5].append((-60 * (i+1), 0))
+			self.moves[6].append((0, 60 * (i+1)))
+			self.moves[7].append((0, -60 * (i+1)))
 
-	def update_moves(self, all_pieces):
-		self.move_rects = []
-		for mov in self.moves:
-			self.move_rects.append(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
+		print(self.moves)
 
 class King(Piece):
 	def __init__(self, position, colour):
 		super().__init__(position, colour)
 
-		self.moves.append((60,0))
-		self.moves.append((-60,0))
-		self.moves.append((0,60))
-		self.moves.append((0,-60))
+		# 8 Possible types of movement
+		self.moves = [
+				[],[],[],[],[],[],[],[]
+			]
 
-	def update_moves(self, all_pieces):
-		self.move_rects = []
-		for mov in self.moves:
-			self.move_rects.append(pygame.Rect(self.piece_rect.x + mov[0], self.piece_rect.y + mov[1], 30, 30))
+		self.moves[0].append((60,0))
+		self.moves[1].append((-60,0))
+		self.moves[2].append((0,60))
+		self.moves[3].append((0,-60))
+
+		self.moves[4].append((60,60))
+		self.moves[5].append((-60,60))
+		self.moves[6].append((60,-60))
+		self.moves[7].append((-60,-60))
+
+		self.textsurf = self.font.render('Ki', False, (0,179,255))
+
